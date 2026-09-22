230c5da5f8f836d8a3c0826133617adc3353331b
Eduardo Reyes <joeduardoreyes@outlook.com>
2026-09-22 10:52:47 -0600

    Lesson 4: Variables and best practices

---
+++ clase-variables.js
@@ -0,0 +1,12 @@
+let cajaDeAnady = "Woody";
+console.log(cajaDeAnady);
+
+// Lo No Permitido
+let c = "Woody";
+let cda = "Woody";
+let pcAndy = "Woody";
+
+// Lo permitido
+let primerTrasteoDeAndy = "Woody";
+let urlDelUsuario = "https://joedu.dev";
+let idDelUsuario = "123456";
