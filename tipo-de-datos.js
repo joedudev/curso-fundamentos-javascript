67998e7babef36b6e8516bd3811c7a384f01b399
Eduardo Reyes <joeduardoreyes@outlook.com>
2026-09-22 11:36:30 -0600

    Lesson 5: Primitive and complex data types

---
+++ tipo-de-datos.js
@@ -0,0 +1,38 @@
+// PRIMITIVOS
+
+// string
+let nombre = "Teresa";
+
+// number
+let edad = 25;
+
+// boolean
+let esMayorDeEdad = true;
+
+// null
+let valorNulo = null;
+
+// undefined
+let valorIndefinido = undefined;
+
+// symbol
+let simbolo = Symbol("simbolo");
+
+// bigint
+let numeroGrande = 9007199254740991n;
+
+// COMPLEJOS
+
+// object
+let carro = {
+	marca: "Tesla",
+	modelo: "Model S",
+};
+
+// array
+let frutas = ["manzana", "banana", "cereza"];
+
+// function
+function saludar() {
+	return "Hola";
+}
