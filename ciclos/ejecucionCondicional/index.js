8155a09195f8e925023af453d504181abde31626
Eduardo Reyes <joeduardoreyes@outlook.com>
2026-09-23 07:37:32 -0600

    Lesson 12: Conditional structures if, else and else if

---
+++ ciclos/ejecucionCondicional/index.js
@@ -0,0 +1,9 @@
+let nombre = "Nico";
+
+if (nombre === "Eduardo") {
+	console.log("Hola Eduardo");
+} else if (nombre === "Nico") {
+	console.log("Hola Nico");
+} else {
+	console.log("Nombre no encontrado");
+}
