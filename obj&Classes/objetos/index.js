7b2932da305689e1bf72c1cb90066cb2fdf761de
Eduardo Reyes <joeduardoreyes@outlook.com>
2026-09-24 09:31:33 -0600

    Lesson 35: Objects, properties and methods

---
+++ obj&Classes/objetos/index.js
@@ -0,0 +1,11 @@
+const persona = {
+	nombre: "John",
+	edad: 30,
+	direccion: {
+		calle: "Avenida Insurgentes 187",
+		ciudad: "CDMX",
+	},
+	saludar() {
+		console.log(`Hola, nombre es ${persona.nombre}`);
+	},
+};
