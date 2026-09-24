b7e5b6f49ff72576c00f340c674e38ae64a00e50
Eduardo Reyes <joeduardoreyes@outlook.com>
2026-09-24 07:24:35 -0600

    Lesson 29: Array mutability with push and pop

---
+++ arrays/clase-push-pop.js
@@ -0,0 +1,16 @@
+// Methos that modify the array
+
+// Push
+
+const countries = ["USA", "CAN", "Uk"];
+const newCountries = countries.push("DT", "AU");
+
+console.log(countries);
+console.log(newCountries);
+
+// Pop
+
+const removedCoutry = countries.pop();
+
+console.log(countries);
+console.log(removedCoutry);
