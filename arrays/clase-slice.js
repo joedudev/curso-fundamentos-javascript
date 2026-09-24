74f42eb68aa4cf732e129c59d7d01e6d6bb8f1cc
Eduardo Reyes <joeduardoreyes@outlook.com>
2026-09-24 09:00:39 -0600

    Lesson 33: Extracting array portions with slice

---
+++ arrays/clase-slice.js
@@ -0,0 +1,9 @@
+// Sllice
+
+const animals = ["Ante", "Bison", "Camel", "Duck", "Elephant"];
+
+console.log(animals.slice(2));
+console.log(animals.slice(2, 4));
+console.log(animals.slice(1, 5));
+console.log(animals.slice(-2));
+console.log(animals.slice(2, -1));
