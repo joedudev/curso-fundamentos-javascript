3c407883ae7203f8af7b7f5decad417468b6c95e
Eduardo Reyes <joeduardoreyes@outlook.com>
2026-09-24 07:19:09 -0600

    Lesson 28: Mutability and immutability in arrays

--- arrays/clase-introduccion-a-array.js
+++ arrays/clase-introduccion-a-array.js
@@ -42,3 +42,28 @@ console.log(firstFruit); // Output: Apple
 
 const numberOfFruits = fruits.length;
 console.log(numberOfFruits);
+
+// Mutability
+
+fruits.push("Watermelon");
+console.log(fruits);
+
+const newfruits = fruits.concat("grape", "Kiwi");
+console.log(fruits);
+console.log(newfruits);
+
+//  Array.isArray
+
+const isArray = Array.isArray(fruits);
+console.log(isArray);
+
+// Paratical, sum of all array elemtns
+
+const numbersArray = [1, 2, 3, 4, 5];
+let sum = 0;
+
+for (let i = 0; i < numbersArray.length; i++) {
+	sum += numbersArray[i];
+}
+
+console.log(sum);
