083c3b4566d8def9d85243bebe28ce9b225159ed
Eduardo Reyes <joeduardoreyes@outlook.com>
2026-09-24 08:36:51 -0600

    Lesson 32: Find and findIndex methods

---
+++ arrays/clase-fin-finIndex.js
@@ -0,0 +1,14 @@
+const multiplesOf5 = [5, 10, 15, 20];
+
+const firstNumberGreaterThan10 = multiplesOf5.find((number) => number > 10);
+
+console.log(multiplesOf5);
+console.log(firstNumberGreaterThan10);
+
+// FinIndex
+
+const randomNumber = [6, 14, 27, 56, 40];
+const indexNumber = randomNumber.findIndex((number) => number > 50);
+
+console.log(randomNumber);
+console.log(indexNumber);
